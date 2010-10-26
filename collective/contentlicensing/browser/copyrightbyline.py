e2722bafc0e4a23b588149ad2e4960f5d8bacaac
blambert <blambert@localhost>
2010-10-26 06:25:00 +0000

    Adding new views.
    svn path=/collective.contentlicensing/trunk/; revision=225256

---
+++ collective/contentlicensing/browser/copyrightbyline.py
@@ -0,0 +1,7 @@
+from plone.app.layout.viewlets import ViewletBase
+
+class CopyrightByline(ViewletBase):
+    """ """
+
+    def __init__(self, context, request, view, manager=None):
+        super(CopyrightByline, self).__init__(context, reqest, view, manager)
