cd279fa0a44ca1c182cecc3456b08d1821e2796f
Dimitris Moraitis <dimo@unweb.me>
2012-05-14 18:16:13 +0300

    Prepare collective.contentlicensing 2.2.4.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.2.3'
+version = '2.2.4'
 tests_require = ['plone.app.blob [test]']
 
 setup(name='collective.contentlicensing',
