3424f6199e13af4cbb900656e3e306c891f84c43
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2014-01-20 22:13:02 -0430

    Updated setuptools metadata options for this package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.2.5'
+version = '2.2.6'
 tests_require = ['plone.app.blob [test]']
 
 setup(name='collective.contentlicensing',
@@ -9,10 +9,17 @@ setup(name='collective.contentlicensing',
       description="This tool is used to manage copyright licenses within plone.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Printing",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='Licensing License Creative-Commons GPL All-Rights-Reserved Copyright Rights',
